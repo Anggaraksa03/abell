3878b9f73426a91d8abf4a277c82b9384162ad0b
alvinmr <maulanaalvin1@gmail.com>
2020-02-19 00:15:46 +0800

    edit template dikit

--- application/views/template/layout.php
+++ application/views/template/layout.php
@@ -14,14 +14,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </form>
         <ul class="navbar-nav navbar-right">
           <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-            <img alt="image" src="<?php echo base_url(); ?>assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+            <img alt="image" src="<?php echo base_url(); ?>assets/img/avatar/avatar-4.png" class="rounded-circle mr-1">
             <div class="d-sm-none d-lg-inline-block">Hi, Reynaldi vis Dustin</div></a>
             <div class="dropdown-menu dropdown-menu-right">
               <div class="dropdown-title">Logged in 5 min ago</div>
-              <a href="<?php echo base_url(); ?>dist/features_profile" class="dropdown-item has-icon">
+              <a href="<?php echo base_url(); ?>" class="dropdown-item has-icon">
                 <i class="far fa-user"></i> Profil
               </a>              
-              <a href="<?php echo base_url(); ?>dist/features_settings" class="dropdown-item has-icon">
+              <a href="<?php echo base_url(); ?>" class="dropdown-item has-icon">
                 <i class="fas fa-cog"></i> Settings
               </a>
               <div class="dropdown-divider"></div>
