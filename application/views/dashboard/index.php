c93f331e7af5afa218b745c54c233018c72cbdea
alvinmr <maulanaalvin1@gmail.com>
2020-02-19 00:03:17 +0800

    edit warna di template

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,1 +1,11 @@
-hello ini dashboard
+<div class="main-content">
+    <div class="section">
+        <div class="section-header">
+        <h1>Dashboard</h1>
+        </div>
+
+        <div class="section-body">
+        test
+        </div>
+    </div>
+</div>
